451f179349f4f1f46dad3694140e7ec893bf633d
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-01 13:35:51 +0800

    Add for loop that will multiply the result to baseNumber

--- exponentMethod/Program.cs
+++ exponentMethod/Program.cs
@@ -7,6 +7,13 @@
 
     static int GetPower(int baseNumber, int powerNumber)
     {
+        int result = 1;
 
+        for (int i = 0; i < powerNumber; i++)
+        {
+            result = result * baseNumber;
+        }
+        
+        return result;
     }
 }
