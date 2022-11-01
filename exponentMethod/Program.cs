571b1b9f1a7cab544fab639b0df3ea72423ac6d7
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-01 13:52:18 +0800

    Add user input

--- exponentMethod/Program.cs
+++ exponentMethod/Program.cs
@@ -3,6 +3,11 @@
     static void Main(string[] args)
     {
         int num1, num2;
+
+        Console.Write("Enter a number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
     }
 
     static int GetPower(int baseNumber, int powerNumber)
